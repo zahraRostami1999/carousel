68e4971f038f2932e835632ca2a809ab077cb379
Zahra Rostami <zahrarostami137878@gmail.com>
2024-12-05 12:27:26 +0330

    edit HomePage: add carousel and set w-full and h-1/3

--- carousel/src/pages/HomePage.jsx
+++ carousel/src/pages/HomePage.jsx
@@ -1,11 +1,19 @@
+import Carousel from '../components/carousel'
+import styles from "./HomeStyles.module.css";
+
 const HomePage = () => {
-    return ( 
+    return (
         <>
-        <div>
-            <h1>HomePage</h1>
-        </div>
+            <div className="w-full h-screen bg-neutral-800">
+                <div className="w-full h-1/3">
+                    <div className={styles.carousel}>
+                        <Carousel />
+                    </div>
+
+                </div>
+            </div>
         </>
-     );
+    );
 }
- 
+
 export default HomePage;
